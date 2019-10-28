5e36aba50b484de2e9ce1075b4d7871d0ed5ba86
serena Lee <dearserenalee@gmail.com>
2019-10-28 04:19:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,12 +11,10 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  my_array = ["cloudy", "snowy", "sunny"]
-  my_array.unshift("rainy")
+  my_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  my_array = ["cloudy", "snowy", "sunny"]
   my_array.pop
 end
 
