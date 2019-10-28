9d8e66496f5e080a55b1a9efc9dbbd65167e7132
serena Lee <dearserenalee@gmail.com>
2019-10-28 04:18:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["cloudy", "rainy", "sunny"]
-  my_array << "snowy"
+  array<<
 end
 
 def add_element_to_start_of_array(array, element)
